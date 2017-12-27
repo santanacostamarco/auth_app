23a6fe1f198ea4d563d164730de5768e6515724c
Marco Antonio <santannacostamarco@gmail.com>
2017-12-27 15:55:52 -0200

    configuracao para login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password #encriptar a senha
+    validates name, presence: true
+    validates password, presence: true
+    VALID_EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+    validates email, presence: true, format: {with: VALID_EMAIL_FORMAT}, uniqueness: {case_sensitive: false}
+    before_save {self.email = email.downcase}
 end
